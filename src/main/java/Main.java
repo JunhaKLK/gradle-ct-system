d114f64df7a61bcc879af56d2ea96e6ad698490d
JunhaKLK <junha0206@naver.com>
2025-01-24 14:34:24 +0900

    add template

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,44 +1,51 @@
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
 
-    // https://www.acmicpc.net/problem/31846
+    // https://www.acmicpc.net/problem/16195
 
     public static void problemSolver() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
+        int t = Integer.parseInt(br.readLine());
 
-        char[] chars = br.readLine().toCharArray();
+        final long mod = 1_000_000_009L;
 
-        int loop = Integer.parseInt(br.readLine());
+        long[][] dp = new long[1001][1001];
+
+        dp[1][1] = 1;
+        dp[1][2] = 1;
+        dp[1][3] = 1;
+
+        dp[2][2] = 1;
+        dp[2][3] = 2;
+        dp[2][4] = 3;
+        dp[2][5] = 2;
+        dp[2][6] = 1;
+
+
+        for (int i = 3; i < 1001; i++) {
+            for (int j = i; j < 1001; j++) {
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j - 2] + dp[i - 1][j - 3];
+                dp[i][j] %= mod;
+            }
+        }
 
         StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < t; i++) {
+            String[] token = br.readLine().split(" ");
+            int n = Integer.parseInt(token[0]);
+            int m = Integer.parseInt(token[1]);
 
-        for (int i = 0; i < loop; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            long r = 0;
 
-            int l = Integer.parseInt(st.nextToken());
-            int r = Integer.parseInt(st.nextToken());
-
-            int max = 0;
-
-            for (int fold = l; fold < r; fold++) {
-                int count = 0;
-                for (int adder = 0; adder <= Math.min((fold - l),(r - fold - 1)); adder++) {
-                    if (chars[fold - adder - 1] == chars[fold + adder]) {
-                        ++count;
-                    }
-                }
-
-                if (count > max) {
-                    max = count;
-                }
+            for (int pos = 0; pos <= m; pos++) {
+                r += dp[pos][n];
+                r %= mod;
             }
 
-            sb.append(Integer.toString(max)).append("\n");
+            sb.append(r).append("\n");
         }
 
         bw.write(sb.substring(0, sb.length() - 1));
@@ -47,7 +54,6 @@ public class Main {
         bw.close();
         br.close();
     }
-
 
     public static void main(String[] args) throws IOException {
         problemSolver();
